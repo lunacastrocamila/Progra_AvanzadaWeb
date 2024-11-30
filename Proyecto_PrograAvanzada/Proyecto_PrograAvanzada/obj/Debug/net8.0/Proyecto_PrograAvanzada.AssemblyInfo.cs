77be42eaa780ef741e4ba5d9ec71e90199bc4362
SaulRet <sretana30764@ufide.ac.cr>
2024-11-30 12:29:42 -0600

    Crud Técnico a español
    Se actualiza el Crud de Técnico a español

--- Proyecto_PrograAvanzada/Proyecto_PrograAvanzada/obj/Debug/net8.0/Proyecto_PrograAvanzada.AssemblyInfo.cs
+++ Proyecto_PrograAvanzada/Proyecto_PrograAvanzada/obj/Debug/net8.0/Proyecto_PrograAvanzada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bc0fbada79e5651ca9f16b18a151d48713739b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ce7895dd93883194e989cea133bc2dcdc7b4d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
