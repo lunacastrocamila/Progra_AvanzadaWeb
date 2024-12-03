b540e255564ce2e5cade5e93ee2ab504ba0cedfa
lunacastrocamila <98502372+lunacastrocamila@users.noreply.github.com>
2024-12-02 21:00:43 -0600

    test

--- Proyecto_PrograAvanzada/Proyecto_PrograAvanzada/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_PrograAvanzada.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
